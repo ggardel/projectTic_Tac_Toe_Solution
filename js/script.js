2fb643650acb7cfd1282655246173b5380a147df
Gustavo Ferreira <gustavogardel@gmai.com>
2016-06-28 12:05:28 -0700

    refactor the code

--- js/script.js
+++ js/script.js
@@ -9,8 +9,20 @@ var turn = 'X'
 
 for (var i=0; i<9; i++){
   squares[i].addEventListener('click', function(e){
+    var squareIndex = getSquareIndex(e.target)
+    if(board[squareIndex]== ""){
+      board[squareIndex] = turn
+      drawBoard()
+      switchTurn()
+    }
+  })
+}
+
+
 //loop through squares again and check which node was clicked
-    for (var j=0; j<9; j++){
+
+
+    /*for (var j=0; j<9; j++){
       if (squares[j] ===e.target && board[j]=== " " ){
         board[j]= turn
         drawBoard()
@@ -18,7 +30,21 @@ for (var i=0; i<9; i++){
       }
     }
   })
+}*/
+
+
+//given an HTML element, it will determine the index of the square on the board
+function getSquareIndex(target){
+  for (var j=0; j < 9 ; j++){
+    if (squares[j] ===target){
+      //check if the current iterated square that was clicked
+      return j;
+    }
+  }
 }
+
+
+
 //drawing the model to screen
 function drawBoard(){
   for (var i=0; i<9; i++){
