ad3750cc54ae18b8d7d5b67a1371aec82f9a9216
Gustavo Ferreira <gustavogardel@gmai.com>
2016-06-28 11:29:30 -0700

    switch turn function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
-var board =['X', 'O', ' ',' ', ' ', 'X', ' ', ' ', ' ']
+//models
+var board =[' ', ' ', ' ',' ', ' ', ' ', ' ', ' ', ' ']
 var turn = 'X'
+//selectors
 
-function drawBoard(){
   var squares = document.querySelectorAll('.square')
+
+//set up click listeners for squares
+
+for (var i=0; i<9; i++){
+  squares[i].addEventListener('click', function(e){
+//loop through squares again and check which node was clicked
+    for (var j=0; j<9; j++){
+      if (squares[j] ===e.target){
+        board[j]= turn
+        drawBoard()
+        switchTurn()
+      }
+    }
+  })
+}
+//drawing the model to screen
+function drawBoard(){
   for (var i=0; i<9; i++){
     squares[i].innerHTML = board[i]
+  }
+}
+
+//switching turns
+function switchTurn(){
+  if (turn === 'X')
+  turn = 'O'
+  else {
+    turn = 'X'
   }
 }
